8c25ee9f4079f2efa82ffc7a7f66f1f4fa0e24fe
Damian Jamróz <mrlorem@gmail.com>
2022-07-07 17:33:20 +0200

    init

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,7 +1,6 @@
-import "./App.css";
 import React from "react";
 
-function App() {
+function Employees() {
 	return;
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
-import "./App.css";
 import React from "react";
 
-function App() {
-	return;
+function Footer() {
+	return <footer />;
 }
 
 export default App;
