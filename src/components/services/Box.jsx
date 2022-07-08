059aea374259df8e1255930f478f47774801447f
Damian Jamróz <mrlorem@gmail.com>
2022-07-08 21:03:28 +0200

    use map to render offerts

--- src/components/services/Box.jsx
+++ src/components/services/Box.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export const Box = () => {
-	return;
+export const Box = ({ imgSrc, imgAlt, title, isNew }) => {
+	return (
+		<div class="box">
+			<img src={imgSrc} alt={imgAlt} />
+			{isNew ? <span class="new"></span> : null}
+			<div>
+				<p>{title}</p>
+			</div>
+		</div>
+	);
 };
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,65 +1,15 @@
 import React from "react";
+import { oferty } from "../../data/oferta";
+import { Box } from "./Box";
 
 export const Services = () => {
 	return (
 		<section id="oferta" class="offer-sec">
 			<h2>Czym zajada się nasza firma?</h2>
 			<div class="offer-sec-box">
-				<div class="box">
-					<img
-						src="https://images.unsplash.com/photo-1610213011891-68167bbe574c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-						alt="Pieróg wariacki"
-					/>
-					<span class="new"></span>
-					<div>
-						<p>Pieróg wariacki *NOWOŚĆ*</p>
-					</div>
-				</div>
-				<div class="box">
-					<img
-						src="https://images.unsplash.com/photo-1589047133481-02b4a5327d89?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-						alt="Pieróg kaszubski"
-					/>
-					<div>
-						<p>Pieróg kaszubski</p>
-					</div>
-				</div>
-				<div class="box">
-					<img
-						src="https://images.unsplash.com/photo-1543198432-a20fa3055570?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-						alt="Pieróg ziemowiecki"
-					/>
-					<div>
-						<p>Pieróg ziemowiecki</p>
-					</div>
-				</div>
-				<div class="box">
-					<img
-						src="https://images.unsplash.com/photo-1513862153653-f8b7324e1779?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=373&q=80"
-						alt="Pieróg surowy"
-					/>
-					<div>
-						<p>Pieróg surowy</p>
-					</div>
-				</div>
-				<div class="box">
-					<img
-						src="https://images.unsplash.com/photo-1523905330026-b8bd1f5f320e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
-						alt="Pieróg ciepły"
-					/>
-					<div>
-						<p>Pieróg ciepły</p>
-					</div>
-				</div>
-				<div class="box">
-					<img
-						src="https://images.unsplash.com/photo-1638502521795-89107ac5e246?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
-						alt="Pieróg podsmażany aż palce lizać"
-					/>
-					<div>
-						<p>Pieróg podsmażany aż palce lizać</p>
-					</div>
-				</div>
+				{oferty.map((el, index) => (
+					<Box key={index} {...el} />
+				))}
 			</div>
 		</section>
 	);
